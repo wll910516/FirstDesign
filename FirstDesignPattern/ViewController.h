64c037e24a8514a185e1b87860d144cad8db7dd3
Xummer <xummer26@gmail.com>
2016-11-25 12:22:42 +0800

    Initial Commit

---
+++ FirstDesignPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstDesignPattern
+//
+//  Created by Xummer on 2016/11/25.
+//  Copyright © 2016年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
